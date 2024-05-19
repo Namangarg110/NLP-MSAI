1275ac178cc8e05ff81d992f47c6b9c643da83d5
CodeMonkeyInShoes <107077525+CodeMonkeyInShoes@users.noreply.github.com>
2024-05-19 11:23:56 -0500

    Pulling the loading and tokenizing code into a method to make it easier to reuse.

--- starter.py
+++ starter.py
@@ -9,93 +9,49 @@ import json
 import numpy as np
 
 
-def main():  
+def load_corpus(file_name, tokenizer):
+    answers = ['A', 'B', 'C', 'D']
+    sequences = []
+
+    with open(file_name) as json_file:
+        json_list = list(json_file)
+    for i in range(len(json_list)):
+        json_str = json_list[i]
+        result = json.loads(json_str)
+
+        base = result['fact1'] + ' [SEP] ' + result['question']['stem']
+        ans = answers.index(result['answerKey'])
+
+        obs = []
+        for j in range(4):
+            text = base + result['question']['choices'][j]['text'] + ' [SEP]'
+            if j == ans:
+                label = 1
+            else:
+                label = 0
+            obs.append([text, label])
+        sequences.append(obs)
+
+    tokenized_sequences = []
+
+    for seq in sequences:
+        tokenized_sequences.append([(tokenizer.tokenize(s[0]), s[1]) for s in seq])
+
+    return tokenized_sequences
+
+
+def main():
     torch.manual_seed(0)
-    answers = ['A','B','C','D']
-
-    train = []
-    test = []
-    valid = []
-    
-    file_name = 'train_complete.jsonl'        
-    with open(file_name) as json_file:
-        json_list = list(json_file)
-    for i in range(len(json_list)):
-        json_str = json_list[i]
-        result = json.loads(json_str)
-        
-        base = result['fact1'] + ' [SEP] ' + result['question']['stem']
-        ans = answers.index(result['answerKey'])
-        
-        obs = []
-        for j in range(4):
-            text = base + result['question']['choices'][j]['text'] + ' [SEP]'
-            if j == ans:
-                label = 1
-            else:
-                label = 0
-            obs.append([text,label])
-        train.append(obs)
-        
-        print(obs)
-        print(' ')
-        
-        print(result['question']['stem'])
-        print(' ',result['question']['choices'][0]['label'],result['question']['choices'][0]['text'])
-        print(' ',result['question']['choices'][1]['label'],result['question']['choices'][1]['text'])
-        print(' ',result['question']['choices'][2]['label'],result['question']['choices'][2]['text'])
-        print(' ',result['question']['choices'][3]['label'],result['question']['choices'][3]['text'])
-        print('  Fact: ',result['fact1'])
-        print('  Answer: ',result['answerKey'])
-        print('  ')
-                
-    file_name = 'dev_complete.jsonl'        
-    with open(file_name) as json_file:
-        json_list = list(json_file)
-    for i in range(len(json_list)):
-        json_str = json_list[i]
-        result = json.loads(json_str)
-        
-        base = result['fact1'] + ' [SEP] ' + result['question']['stem']
-        ans = answers.index(result['answerKey'])
-        
-        obs = []
-        for j in range(4):
-            text = base + result['question']['choices'][j]['text'] + ' [SEP]'
-            if j == ans:
-                label = 1
-            else:
-                label = 0
-            obs.append([text,label])
-        valid.append(obs)
-        
-    file_name = 'test_complete.jsonl'        
-    with open(file_name) as json_file:
-        json_list = list(json_file)
-    for i in range(len(json_list)):
-        json_str = json_list[i]
-        result = json.loads(json_str)
-        
-        base = result['fact1'] + ' [SEP] ' + result['question']['stem']
-        ans = answers.index(result['answerKey'])
-        
-        obs = []
-        for j in range(4):
-            text = base + result['question']['choices'][j]['text'] + ' [SEP]'
-            if j == ans:
-                label = 1
-            else:
-                label = 0
-            obs.append([text,label])
-        test.append(obs)
-
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
     model = BertModel.from_pretrained("bert-base-uncased")
+
+    train = load_corpus('train_complete.jsonl', tokenizer)
+    test = load_corpus('dev_complete.jsonl', tokenizer)
+    valid = load_corpus('test_complete.jsonl', tokenizer)
+
     optimizer = optim.Adam(model.parameters(), lr=3e-5)
-    linear = torch.rand(768,2)
-    
-#    Add code to fine-tune and test your MCQA classifier.
-           
-                 
+    linear = torch.rand(768, 2)
+
+
 if __name__ == "__main__":
     main()
